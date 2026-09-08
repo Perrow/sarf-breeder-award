ebe00a29c805cada5935f5d906cd619ac03a0c5e
Perrow <per.trelje@gmail.com>
2026-09-08 22:47:53 +0200

    BA-101V: validate association competition limits

---
+++ breedings/test_association_competition_limit_validation.py
@@ -0,0 +1,66 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from taxonomy.models import Genus, Species, SpeciesGroup
+
+from .models import AssociationCompetitionLimit
+
+
+class AssociationCompetitionLimitValidationTests(TestCase):
+    def setUp(self):
+        self.genus = Genus.objects.create(scientific_name="Corydoras")
+        self.species = Species.objects.create(
+            genus=self.genus,
+            scientific_name="aeneus",
+            common_name="Metallpansarmal",
+            breeding_class=Species.BreedingClass.BRONZE,
+        )
+
+    def test_rule_requires_exactly_one_taxonomy_target(self):
+        with self.assertRaises(ValidationError):
+            AssociationCompetitionLimit.objects.create(max_registrations_per_member=5)
+
+        group = SpeciesGroup.objects.create(name="Pansarmalar")
+        group.genera.add(self.genus)
+        with self.assertRaises(ValidationError):
+            AssociationCompetitionLimit.objects.create(
+                genus=self.genus,
+                species_group=group,
+                max_registrations_per_member=5,
+            )
+
+    def test_rule_requires_positive_maximum(self):
+        with self.assertRaises(ValidationError):
+            AssociationCompetitionLimit.objects.create(
+                genus=self.genus,
+                max_registrations_per_member=0,
+            )
+
+    def test_species_group_with_direct_species_cannot_be_used(self):
+        group = SpeciesGroup.objects.create(name="Direktarter")
+        group.species.add(self.species)
+
+        with self.assertRaises(ValidationError):
+            AssociationCompetitionLimit.objects.create(
+                species_group=group,
+                max_registrations_per_member=5,
+            )
+
+    def test_genus_rule_is_valid(self):
+        rule = AssociationCompetitionLimit.objects.create(
+            genus=self.genus,
+            max_registrations_per_member=5,
+        )
+
+        self.assertEqual(rule.max_registrations_per_member, 5)
+
+    def test_species_group_with_only_genera_is_valid(self):
+        group = SpeciesGroup.objects.create(name="Pansarmalar")
+        group.genera.add(self.genus)
+
+        rule = AssociationCompetitionLimit.objects.create(
+            species_group=group,
+            max_registrations_per_member=5,
+        )
+
+        self.assertEqual(rule.species_group, group)
