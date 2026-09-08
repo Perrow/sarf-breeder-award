eba0496b11835fb5016172331286eb9961bb67b2
Perrow <per.trelje@gmail.com>
2026-09-08 16:14:15 +0200

    BA-042: support free-text taxonomy

--- breedings/forms.py
+++ breedings/forms.py
@@ -9,10 +9,21 @@ from .models import BreedingRegistration
 class BreedingRegistrationForm(forms.ModelForm):
     class Meta:
         model = BreedingRegistration
-        fields = ("association", "species", "breeding_date", "description")
+        fields = (
+            "association",
+            "species",
+            "proposed_genus_name",
+            "proposed_species_name",
+            "proposed_common_name",
+            "breeding_date",
+            "description",
+        )
         labels = {
             "association": "Förening",
             "species": "Art",
+            "proposed_genus_name": "Släkte i fritext",
+            "proposed_species_name": "Art i fritext",
+            "proposed_common_name": "Populärnamn i fritext",
             "breeding_date": "Odlingsdatum",
             "description": "Beskrivning",
         }
@@ -23,7 +34,6 @@ class BreedingRegistrationForm(forms.ModelForm):
 
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["association"].queryset = Association.objects.filter(
-            memberships__user=user
-        ).distinct()
+        self.fields["association"].queryset = Association.objects.filter(memberships__user=user).distinct()
         self.fields["species"].queryset = Species.objects.available_for_registration()
+        self.fields["species"].required = False
