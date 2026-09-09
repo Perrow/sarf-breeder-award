57ee2fd2b6d280e24d5ec619bb1d29e963390df0
Perrow <per.trelje@gmail.com>
2026-09-09 19:01:58 +0200

    BA-010A: authenticate login by email

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from associations.models import Association, Membership
@@ -90,6 +91,27 @@ class RegistrationForm(UserCreationForm):
 
 class EmailAuthenticationForm(AuthenticationForm):
     username = forms.EmailField(label="E-post")
+
+    def clean(self):
+        email = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+
+        if email is not None and password:
+            users = User.objects.filter(email__iexact=email)
+            if users.count() != 1:
+                raise self.get_invalid_login_error()
+
+            user = users.first()
+            self.user_cache = authenticate(
+                self.request,
+                username=user.get_username(),
+                password=password,
+            )
+            if self.user_cache is None:
+                raise self.get_invalid_login_error()
+            self.confirm_login_allowed(self.user_cache)
+
+        return self.cleaned_data
 
 
 class ProfileForm(forms.ModelForm):
