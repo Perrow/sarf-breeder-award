5aa2053be3e17f4fa036674bced0e9717ff57dbf
Perrow <per.trelje@gmail.com>
2026-09-11 16:14:19 +0200

    Update current-year association scoring expectation

--- breedings/test_association_scoring.py
+++ breedings/test_association_scoring.py
@@ -44,7 +44,7 @@ class AssociationYearScoringTests(TestCase):
         scores = association_year_scores(self.association, 2026)
 
         self.assertEqual(scores[self.user_a.pk], 2)
-        self.assertEqual(scores[self.user_b.pk], 3)
+        self.assertEqual(scores[self.user_b.pk], 2)
 
     def test_other_association_and_unapproved_registrations_are_excluded(self):
         self.add_registration(self.user_a, self.other_association, 2026, Species.BreedingClass.GOLD)
