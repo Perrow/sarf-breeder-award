1553abe49a5be6be8a44fc665c33d3a04c0cf631
Perrow <per.trelje@gmail.com>
2026-09-11 08:14:40 +0200

    BA-040D använd redigeringsflöde för direktgodkänd brons

--- breedings/urls.py
+++ breedings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import species_search, views
+from . import species_search, user_actions, views
 
 urlpatterns = [
     path("topplistor/", views.leaderboards, name="leaderboards"),
@@ -28,6 +28,6 @@ urlpatterns = [
     path("odlingar/artsok/", species_search.species_search_results, name="species_search_results"),
     path("odlingar/ny/", views.breeding_create, name="breeding_create"),
     path("odlingar/<int:pk>/", views.breeding_detail, name="breeding_detail"),
-    path("odlingar/<int:pk>/redigera/", views.breeding_edit, name="breeding_edit"),
+    path("odlingar/<int:pk>/redigera/", user_actions.breeding_edit, name="breeding_edit"),
     path("odlingar/<int:pk>/aterga-till-utkast/", views.breeding_return_to_draft, name="breeding_return_to_draft"),
 ]
