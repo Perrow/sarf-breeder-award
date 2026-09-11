46318474593cf19640b32df5827afaa03db55f06
Perrow <per.trelje@gmail.com>
2026-09-11 12:13:59 +0200

    BA-040H tillåt redigering av alla egna odlingar

--- breedings/user_actions.py
+++ breedings/user_actions.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 from django.shortcuts import get_object_or_404
 
 from taxonomy.models import Species
@@ -16,6 +15,14 @@ def _is_auto_approved_bronze(registration):
     )
 
 
+def _requires_approval_reset(registration):
+    return (
+        registration.status == BreedingRegistration.Status.APPROVED
+        and registration.awarded_breeding_class
+        in {Species.BreedingClass.SILVER, Species.BreedingClass.GOLD}
+    )
+
+
 @login_required
 def breeding_edit(request, pk):
     registration = get_object_or_404(
@@ -24,19 +31,20 @@ def breeding_edit(request, pk):
         owner=request.user,
     )
 
-    if registration.status == BreedingRegistration.Status.DRAFT:
-        return views._edit_breeding(request, registration)
+    if _is_auto_approved_bronze(registration):
+        if request.method == "POST":
+            post_data = request.POST.copy()
+            post_data["action"] = "submit"
+            request.POST = post_data
 
-    if not _is_auto_approved_bronze(registration):
-        raise Http404
-
-    if request.method == "POST":
-        post_data = request.POST.copy()
-        post_data["action"] = "submit"
-        request.POST = post_data
+        return views._edit_breeding(
+            request,
+            registration,
+            selected_species=registration.species,
+        )
 
     return views._edit_breeding(
         request,
         registration,
-        selected_species=registration.species,
+        reset_approval_on_save=_requires_approval_reset(registration),
     )
