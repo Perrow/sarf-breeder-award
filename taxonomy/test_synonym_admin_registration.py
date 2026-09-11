09ec3e957232cbd9f823ffcd791daf6635174c81
Perrow <per.trelje@gmail.com>
2026-09-11 17:11:21 +0200

    BA-026N update synonym admin registration tests

--- taxonomy/test_synonym_admin_registration.py
+++ taxonomy/test_synonym_admin_registration.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.test import SimpleTestCase
 
 from .admin import CommonNameSynonymInline, ScientificSynonymInline, SpeciesAdmin
-from .models import Species, SpeciesSynonym
+from .models import CommonNameSpeciesSynonym, ScientificSpeciesSynonym, Species
 
 
 class SynonymAdminRegistrationTests(SimpleTestCase):
-    def test_species_synonym_is_not_registered_as_separate_admin_model(self):
-        self.assertFalse(admin.site.is_registered(SpeciesSynonym))
+    def test_synonym_models_are_not_registered_as_separate_admin_models(self):
+        self.assertFalse(admin.site.is_registered(ScientificSpeciesSynonym))
+        self.assertFalse(admin.site.is_registered(CommonNameSpeciesSynonym))
 
     def test_species_admin_keeps_separate_synonym_inlines(self):
         model_admin = admin.site._registry[Species]
