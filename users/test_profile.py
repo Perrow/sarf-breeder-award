0e48521412c5819f7ff4696b57e62f551b2cef47
Perrow <per.trelje@gmail.com>
2026-09-11 08:25:11 +0200

    BA-012C uppdatera profiltester för publikt användarnamn

--- users/test_profile.py
+++ users/test_profile.py
@@ -16,27 +16,26 @@ class UserProfileTests(TestCase):
         self.client.force_login(self.user)
 
     def test_account_page_is_read_only_and_links_to_edit(self):
-        self.user.display_name = "Akvaristen"
         self.user.location = "Uppsala"
         self.user.avatar_url = "https://example.com/avatar.jpg"
         self.user.save()
 
         response = self.client.get(reverse("account"))
 
-        self.assertContains(response, "Akvaristen")
         self.assertContains(response, "ProfileUser")
         self.assertContains(response, "Uppsala")
         self.assertContains(response, "https://example.com/avatar.jpg")
         self.assertContains(response, reverse("account_edit"))
-        self.assertNotContains(response, 'name="display_name"')
+        self.assertNotContains(response, "Visningsnamn")
         self.assertNotContains(response, 'name="public_username"')
 
     def test_user_can_update_own_profile_on_edit_page(self):
         response = self.client.post(
             reverse("account_edit"),
             {
+                "first_name": "Test",
+                "last_name": "Person",
                 "public_username": "PellePublic",
-                "display_name": "Pelle",
                 "location": "Uppsala",
                 "avatar_url": "https://example.com/pelle.jpg",
             },
@@ -45,14 +44,22 @@ class UserProfileTests(TestCase):
         self.assertRedirects(response, reverse("account"))
         self.user.refresh_from_db()
         self.assertEqual(self.user.public_username, "PellePublic")
-        self.assertEqual(self.user.display_name, "Pelle")
         self.assertEqual(self.user.location, "Uppsala")
         self.assertEqual(self.user.avatar_url, "https://example.com/pelle.jpg")
 
         account_response = self.client.get(reverse("account"))
         self.assertContains(account_response, "PellePublic")
-        self.assertContains(account_response, "Pelle")
         self.assertContains(account_response, "Uppsala")
+        self.assertNotContains(account_response, "Visningsnamn")
+
+    def test_public_display_name_never_falls_back_to_private_name(self):
+        self.user.public_username = None
+        self.user.first_name = "Hemligt"
+        self.user.last_name = "Namn"
+        self.user.save(update_fields=("public_username", "first_name", "last_name"))
+
+        self.assertEqual(self.user.public_display_name(), "Användare")
+        self.assertEqual(self.user.public_display_name(profile_information_is_public=True), "Användare")
 
     def test_account_and_edit_require_login(self):
         self.client.logout()
