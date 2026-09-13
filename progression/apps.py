53d38f44dde0b23eeb21b7cbc765f1c748e5d11a
Perrow <per.trelje@gmail.com>
2026-09-13 13:57:04 +0200

    Register manual award admin

--- progression/apps.py
+++ progression/apps.py
@@ -5,3 +5,6 @@ class ProgressionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "progression"
     verbose_name = "Utmärkelser"
+
+    def ready(self):
+        from . import manual_awards_admin  # noqa: F401
