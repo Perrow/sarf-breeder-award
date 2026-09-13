94de4a1d4a99bfd322dd499dcd4d4fa21d46bffe
Perrow <per.trelje@gmail.com>
2026-09-14 00:09:23 +0200

    Visa endast Utmärkelser i progression-adminmenyn

--- progression/apps.py
+++ progression/apps.py
@@ -5,3 +5,21 @@ class ProgressionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "progression"
     verbose_name = "Utmärkelser"
+
+    def ready(self):
+        from django.contrib import admin
+
+        previous_get_app_list = admin.site.get_app_list
+
+        def get_app_list(request, app_label=None):
+            app_list = previous_get_app_list(request, app_label)
+            for app in app_list:
+                if app["app_label"] == self.label:
+                    app["models"] = [
+                        model
+                        for model in app["models"]
+                        if model["object_name"] == "Achievement"
+                    ]
+            return app_list
+
+        admin.site.get_app_list = get_app_list
