7775aad4b9444ebd4788b63183f1a07d4d7c6ec4
Perrow <per.trelje@gmail.com>
2026-09-13 22:33:57 +0200

    Rename selfmade badge routes

--- progression/urls.py
+++ progression/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import award_demerit_badge, demerit_badges
+from .views import award_selfmade_badge, selfmade_badges
 
 urlpatterns = [
-    path("utmarkelser/demerit/", demerit_badges, name="demerit_badges"),
+    path("utmarkelser/selfmade/", selfmade_badges, name="selfmade_badges"),
     path(
-        "utmarkelser/demerit/<int:badge_id>/tilldela/",
-        award_demerit_badge,
-        name="award_demerit_badge",
+        "utmarkelser/selfmade/<int:badge_id>/tilldela/",
+        award_selfmade_badge,
+        name="award_selfmade_badge",
     ),
 ]
