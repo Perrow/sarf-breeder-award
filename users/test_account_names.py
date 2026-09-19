c402c7172680172c9fd7e93b4ce9b672072ff62e
Perrow <per.trelje@gmail.com>
2026-09-19 23:49:39 +0200

    USER-402 Update private name tests

--- users/test_account_names.py
+++ users/test_account_names.py
@@ -7,19 +7,16 @@ class AccountNameTests(TestCase):
     def setUp(self):
         User = get_user_model()
         self.user = User.objects.create_user(
-            username="member@example.com",
             email="member@example.com",
             password="test-password-123",
-            first_name="Anna",
-            last_name="Andersson",
+            name="Anna Andersson",
             public_username="akvaristen",
         )
         self.client.force_login(self.user)
 
     def _profile_data(self, **overrides):
         data = {
-            "first_name": "Eva",
-            "last_name": "Eriksson",
+            "name": "Eva Eriksson",
             "public_username": "akvaristen",
             "location": "",
             "avatar_url": "",
@@ -28,45 +25,35 @@ class AccountNameTests(TestCase):
         data.update(overrides)
         return data
 
-    def test_account_page_shows_first_and_last_name_and_explanation(self):
+    def test_account_page_shows_private_name_and_explanation(self):
         response = self.client.get(reverse("account"))
 
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Förnamn")
-        self.assertContains(response, "Anna")
-        self.assertContains(response, "Efternamn")
-        self.assertContains(response, "Andersson")
-        self.assertContains(response, "visas bara för administratörer")
-        self.assertContains(response, "kan ta bort ditt medlemskap")
+        self.assertContains(response, "Namn")
+        self.assertContains(response, "Anna Andersson")
+        self.assertContains(response, "Det visas inte publikt")
 
-    def test_account_edit_can_change_first_and_last_name(self):
+    def test_account_edit_can_change_name(self):
         response = self.client.post(reverse("account_edit"), self._profile_data())
 
         self.assertRedirects(response, reverse("account"))
         self.user.refresh_from_db()
-        self.assertEqual(self.user.first_name, "Eva")
-        self.assertEqual(self.user.last_name, "Eriksson")
+        self.assertEqual(self.user.name, "Eva Eriksson")
 
-    def test_first_and_last_name_are_optional(self):
+    def test_name_is_optional_on_profile(self):
         response = self.client.post(
             reverse("account_edit"),
-            self._profile_data(first_name="", last_name=""),
+            self._profile_data(name=""),
         )
 
         self.assertRedirects(response, reverse("account"))
         self.user.refresh_from_db()
-        self.assertEqual(self.user.first_name, "")
-        self.assertEqual(self.user.last_name, "")
+        self.assertEqual(self.user.name, "")
 
-    def test_edit_page_shows_private_name_explanation_once_before_first_name(self):
+    def test_edit_page_explains_private_name_and_public_username(self):
         response = self.client.get(reverse("account_edit"))
 
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Förnamn")
-        self.assertContains(response, "Efternamn")
-        self.assertContains(response, "frivilliga", count=1)
-        self.assertContains(response, "visas bara för administratörer", count=1)
-        self.assertContains(response, "kan ta bort ditt medlemskap", count=1)
-
-        content = response.content.decode()
-        self.assertLess(content.index("För- och efternamn är frivilliga"), content.index("Förnamn"))
+        self.assertContains(response, "Namn")
+        self.assertContains(response, "Användarnamn")
+        self.assertContains(response, "Endast användarnamnet visas publikt")
